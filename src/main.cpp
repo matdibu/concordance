1844ce9e4b778879451339d6862906142264cefc
Matei Dibu <matdibu@protonmail.com>
2022-05-14 17:46:33 +0300

    add include algo (#2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "config.h"
 
-#include <iostream>
 #include <algorithm>
+#include <cstdio>
+#include <iostream>
 
 int main()
 {
-    std::cout << "version (MAJ.MIN): " << concordance_VERSION_MAJOR << '.' << concordance_VERSION_MINOR << std::endl;
+    std::cout << "version is " << concordance_VERSION_MAJOR << "." << concordance_VERSION_MINOR << std::endl;
     return 0;
 }
