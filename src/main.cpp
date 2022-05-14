aa7dcfd6f858f5134ee1c484c5bbdf671ad820ff
Matei Dibu <matdibu@protonmail.com>
2022-05-14 16:42:57 +0300

    add minor version printing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@
 
 int main()
 {
-    std::cout << "version: " << concordance_VERSION_MAJOR << std::endl;
+    std::cout << "version: " << concordance_VERSION_MAJOR << '.' << concordance_VERSION_MINOR << std::endl;
     return 0;
 }
