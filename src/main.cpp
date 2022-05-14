ddeb0cfb1c00fcaa7b30815c6ba36b79bbd46b37
Matei Dibu <matdibu@protonmail.com>
2022-05-14 22:02:02 +0300

    added basic printing

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,21 @@
 #include "config.h"
 
 static int
-generate_concordance(const std::filesystem::path& path_input) 
+generate_concordance(const std::filesystem::path& path_input)
 {
-    (void) path_input;
+  std::ifstream file_input(path_input);
+  std::string word;
+  while (file_input >> word) {
+    std::cout << word << std::endl;
+  }
   return 0;
 }
 
 static int
-verify_concordance(const std::filesystem::path& path_input,
-                   const std::filesystem::path& path_expected)
+verify_concordance(const std::filesystem::path& path_input, const std::filesystem::path& path_expected)
 {
-    (void)path_input;
-    (void)path_expected;
+  (void)path_input;
+  (void)path_expected;
   return 0;
 }
 
@@ -28,14 +31,12 @@ main(int argc, char* argv[])
     if (argc < 2) {
       std::cerr << "no input file given" << std::endl;
     } else if (argc == 2) {
-      std::filesystem::path input(argv[1]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-      ret = generate_concordance(input);
+      std::filesystem::path path_input(argv[1]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      ret = generate_concordance(path_input);
     } else if (argc == 3) {
-      std::filesystem::path input(
-        argv[1]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-      std::filesystem::path expected(
-        argv[2]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-      ret = verify_concordance(input, expected);
+      std::filesystem::path path_input(argv[1]);    // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      std::filesystem::path path_expected(argv[2]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+      ret = verify_concordance(path_input, path_expected);
     }
   } catch (const std::exception& exc) {
     std::cerr << "exception in main " << exc.what() << std::endl;
