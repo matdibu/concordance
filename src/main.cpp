9ab55ad4a18520002f96a664ba23d1faa02df255
Matei Dibu <matdibu@protonmail.com>
2022-05-15 20:33:39 +0300

    Add problem_statement test (#7)

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -5,81 +6,171 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 #include "config.h"
 
+using sentence_freq_t = std::map<std::size_t, std::size_t>;
+using occurance_map_t = std::map<std::string, sentence_freq_t>;
+
 static std::string
-occurs_to_comma_list(const std::map<std::size_t, std::size_t>& occur_map)
+occurs_to_comma_list(const sentence_freq_t& occurance_map)
 {
   std::ostringstream result_stream;
 
   bool is_first = true;
-  for (const auto& map_pair : occur_map) {
-      const auto sentence_no = map_pair.first;
-    if (!is_first) {
-      result_stream << "," << sentence_no;
-    } else {
-      result_stream << sentence_no;
-      is_first = false;
+  for (const auto& sentence_occurances_pair : occurance_map) {
+
+    const auto sentence_no = sentence_occurances_pair.first;
+    auto occurences = sentence_occurances_pair.second;
+
+    while (occurences-- > 0) {
+      if (!is_first) {
+        result_stream << "," << sentence_no;
+      } else {
+        result_stream << sentence_no;
+        is_first = false;
+      }
     }
   }
 
   return result_stream.str();
 }
 
+static bool
+is_sententce_transition(const std::string& lhs, const std::string& rhs)
+{
+  if (lhs.ends_with('.')) {
+    if (!rhs.empty()) {
+      if (0 != std::isupper(rhs.at(0))) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+[[nodiscard]] static std::string
+to_lower(const std::string& string)
+{
+  std::string result(string.length(), '\0');
+
+  std::transform(string.begin(), string.end(), result.begin(), [](unsigned char chr) {
+    return std::tolower(chr);
+  });
+
+  return result;
+}
+
+static occurance_map_t
+file_to_occurance_map(std::ifstream& file_input)
+{
+  occurance_map_t occurance_map;
+
+  std::string word;
+  std::string next;
+  std::size_t sentence_count = 1;
+
+  file_input >> word;
+  while (file_input >> next) {
+
+    const bool is_sententce_tran = is_sententce_transition(word, next);
+
+    word = to_lower(word);
+
+    if (is_sententce_tran || word.ends_with(',') || word.ends_with(':')) {
+      word.erase(word.length() - 1);
+    }
+
+    occurance_map[word][sentence_count]++;
+
+    if (is_sententce_tran) {
+      sentence_count++;
+    }
+
+    word = next;
+  }
+
+  if (!next.empty()) {
+    if (next.ends_with('.')) {
+      next.erase(next.length() - 1);
+    }
+    occurance_map[next][sentence_count]++;
+  }
+
+  return occurance_map;
+}
+
+static std::size_t
+count_occurances(const sentence_freq_t& occurance_map)
+{
+  std::size_t total_occurrances = 0;
+
+  for (const auto sentence_freq_pair : occurance_map) {
+    total_occurrances += sentence_freq_pair.second;
+  }
+
+  return total_occurrances;
+}
+
+static std::ostream& // NOLINT(fuchsia-overloaded-operator)
+operator<<(std::ostream& ostream, const occurance_map_t::value_type& occurance_pair)
+{
+  const auto word = occurance_pair.first;
+  const auto occurances = count_occurances(occurance_pair.second);
+  const auto occurance_list = occurs_to_comma_list(occurance_pair.second);
+
+  ostream << word << " {" << occurances << ":" << occurance_list << "}";
+
+  return ostream;
+}
+
 static std::string
-gen_con(const std::filesystem::path& path_input)
+occurance_map_to_string(const occurance_map_t& occurance_map)
+{
+  std::ostringstream result_stream;
+
+  for (const auto& occurance_pair : occurance_map) {
+    result_stream << occurance_pair << std::endl;
+  }
+
+  return result_stream.str();
+}
+
+static std::string
+file_to_concordance_string(std::ifstream& file_input)
+{
+  auto occur = file_to_occurance_map(file_input);
+
+  return occurance_map_to_string(occur);
+}
+
+static std::string
+path_to_concordance_string(const std::filesystem::path& path_input)
 {
   if (!std::filesystem::exists(path_input)) {
     throw std::runtime_error("input file path is invalid");
   }
 
-  std::map<std::string, std::map<std::size_t, std::size_t>> occur;
-
   std::ifstream file_input(path_input);
 
-  std::string prev;
-  std::string word;
-  std::size_t sentence_count = 1;
-  while (file_input >> word) {
-
-    if (!prev.empty() && !word.empty()) {
-      if (prev.ends_with('.')) {
-        if (0 != std::isupper(word.at(0))) {
-          sentence_count++;
-        }
-      }
-    }
-
-    occur[word][sentence_count]++;
-
-    prev = word;
-  }
-
-  std::ostringstream result_stream;
-  for (const auto& occ : occur) {
-    std::size_t total_occur = 0;
-    for (const auto freq : occ.second) {
-      total_occur += freq.second;
-    }
-    result_stream << occ.first << " {" << total_occur << ":" << occurs_to_comma_list(occ.second) << "}" << std::endl;
-  }
-
-  return result_stream.str();
+  return file_to_concordance_string(file_input);
 }
 
 static int
-generate_concordance(const std::filesystem::path& path_input)
+one_arg(const std::filesystem::path& path_input)
 {
-  auto result = gen_con(path_input);
+  auto result = path_to_concordance_string(path_input);
   std::cout << "gen_con result: " << std::endl << result << std::endl;
   return 0;
 }
 
 static int
-verify_concordance(const std::filesystem::path& path_input, const std::filesystem::path& path_expected)
+two_arg(const std::filesystem::path& path_input,
+        const std::filesystem::path& path_expected)
 {
-  auto computed_str = gen_con(path_input);
+  auto computed_str = path_to_concordance_string(path_input);
   std::cout << "gen_con result: " << std::endl << computed_str << std::endl;
   auto computed = std::istringstream(computed_str);
 
@@ -89,8 +180,10 @@ verify_concordance(const std::filesystem::path& path_input, const std::filesyste
   std::ifstream file_expected(path_expected);
 
   file_expected.seekg(0, std::ifstream::end);
-  if (const auto file_size = static_cast<std::size_t>(file_expected.tellg()); file_size != computed_str.length()) {
-    std::cout << "file_expected:" << file_size << " computed_str:" << computed_str.length() << std::endl;
+  if (const auto file_size = static_cast<std::size_t>(file_expected.tellg());
+      file_size != computed_str.length()) {
+    std::cout << "file_expected:" << file_size
+              << " computed_str:" << computed_str.length() << std::endl;
     return -1;
   }
   file_expected.seekg(0, std::ifstream::beg);
@@ -106,16 +199,17 @@ int
 main(int argc, char* argv[])
 {
   int ret = 0;
+
   try {
-    if (argc < 2) {
+
+    std::vector<std::string> args(argv, argv + argc);
+
+    if (args.size() < 2) {
       std::cerr << "no input file given" << std::endl;
-    } else if (argc == 2) {
-      std::filesystem::path path_input(argv[1]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-      ret = generate_concordance(path_input);
-    } else if (argc == 3) {
-      std::filesystem::path path_input(argv[1]);    // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-      std::filesystem::path path_expected(argv[2]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-      ret = verify_concordance(path_input, path_expected);
+    } else if (args.size() == 2) {
+      ret = one_arg(args[1]);
+    } else if (args.size() == 3) {
+      ret = two_arg(args[1], args[2]);
     }
   } catch (const std::ios_base::failure& fail) {
     std::cout << "exc code:" << fail.code() << " : " << fail.what() << std::endl;
@@ -123,8 +217,6 @@ main(int argc, char* argv[])
     std::cerr << "exception in main " << exc.what() << std::endl;
     ret = -1;
   }
-
-  std::cout << "returing " << ret << std::endl;
 
   return ret;
 }
